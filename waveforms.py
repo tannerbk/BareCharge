55592d8f8437e54069501cdb794fac9372b46bf1
Tanner Kaptanoglu <tannerk@sas.upenn.edu>
2016-07-04 10:25:31 -0400

    small updates

--- waveforms.py
+++ waveforms.py
@@ -4,28 +4,15 @@ import numpy as np
 import h5py
 import ROOT
     
-
-'''  Get the waveform attributes
-   : param filename:   data file
-   : param channel:    data channel
-     Returns number of samples, number of waveforms
-     and horizontal resolution
-'''
 def read_hdf5(channel):
     samples = len(channel[0])
     waveforms = len(channel)
     dx = channel.attrs['horiz_interval']
     return samples, waveforms, dx
 
-'''  Draw waveforms with the debug option 
-   : param i:            waveform number 
-   : param samples:      number of samples
-   : param channel:      channel
-     Returns TH1 Waveform of scope trace
-'''
-
 def draw_waveform(i, samples, dx, channel):
 
+    canvas = ROOT.TCanvas()
     Waveform = ROOT.TH1F('waveform',';t (ns);ADC Counts',samples,0,samples*dx*1e9)
     for j in range(0, samples):
         adc = channel[i,j]
@@ -35,33 +22,23 @@ def draw_waveform(i, samples, dx, channel):
     print i
     yaxis = ("ACD Counts")
     Waveform.Draw("e")
+    canvas.Update()
     Waveform.SetStats(0)
     Waveform.SetLineColor(ROOT.kViolet)
-    canvas.Update()
 
     raw_input()
 
-''' In debug mode this calls draw_waveform, which will continue to
-    print waveforms to the screen. Cancel with CTRL-C.
-'''
 def plot(filename, channel):
 
     f = h5py.File(filename)
     channel = f[channel]
     samples, waveforms, dx = read_hdf5(channel)
 
-    global canvas
-    canvas = ROOT.TCanvas()
-
     for i in range(0, waveforms):
         draw_waveform(i, samples, dx, channel)
 
     return 0
 
-''' Input arguments to process
-   : arg filename:        data file to process
-   : arg channel:         channel to process
-'''
 if (__name__=="__main__"):
     import argparse
 
