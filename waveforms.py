03cfc17c6cf9bac3dc4ade1a379be2e6f406e8db
Tanner Kaptanoglu <tannerk@sas.upenn.edu>
2017-07-08 15:15:27 -0400

    update waveform plotting scipt

--- waveforms.py
+++ waveforms.py
@@ -1,45 +1,56 @@
 #!/usr/bin/env python2.7
 
-import numpy as np
 import h5py
 import ROOT
     
 def read_hdf5(channel):
-    samples = len(channel[0])
-    waveforms = len(channel)
-    dx = channel.attrs['horiz_interval']
-    return samples, waveforms, dx
+    ''' 
+    Read the hdf5 information
+    '''
 
-def draw_waveform(i, samples, dx, channel):
+    samples = len(channel[0])
+    dx = channel.attrs['horiz_interval']
+    dy = channel.attrs['vertical_gain']
+    return samples, dx, dy
+
+def draw_waveform(i, samples, dx, dy, channel):
+    '''
+    Draw and individual scope waveform using ROOT
+    '''
 
     canvas = ROOT.TCanvas()
-    Waveform = ROOT.TH1F('waveform',';t (ns);ADC Counts',samples,0,samples*dx*1e9)
+    Waveform = ROOT.TH1F('waveform',';t (ns);Voltage(V)',samples,0,samples*dx*1e9)
     for j in range(0, samples):
         adc = channel[i,j]
-        Waveform.SetBinContent(j+1,adc)
-        Waveform.SetBinError(j+1,1)
-    print "Drawing waveform",
-    print i
-    yaxis = ("ACD Counts")
-    Waveform.Draw("e")
-    canvas.Update()
+        Waveform.SetBinContent(j+1,adc*dy)
+        # One ADC-Count error bars
+        Waveform.SetBinError(j+1,dy)
     Waveform.SetStats(0)
-    Waveform.SetLineColor(ROOT.kViolet)
-
+    Waveform.SetLineColor(ROOT.kBlack)
+    Waveform.Draw("")
+    canvas.Update()
     raw_input()
 
 def plot(filename, channel):
+    '''
+    Open the hdf5 file and plot each waveform
+    '''
 
     f = h5py.File(filename)
     channel = f[channel]
-    samples, waveforms, dx = read_hdf5(channel)
+    samples, dx, dy = read_hdf5(channel)
+    waveform_id = 0
 
-    for i in range(0, waveforms):
-        draw_waveform(i, samples, dx, channel)
+    print "Drawing Waveforms, type 'CTRL-C' to exit."
+    while True:
+        print "Drawing waveform", waveform_id
+        draw_waveform(waveform_id, samples, dx, dy, channel)
+        waveform_id+=1
 
     return 0
 
 if (__name__=="__main__"):
+ 
     import argparse
 
     parser = argparse.ArgumentParser(description='Draw waveforms')
